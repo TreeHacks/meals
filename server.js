a0ede841c92009b6cffed4a7bd5a999b880c195d
Irfan Nafi <49328787+inafi@users.noreply.github.com>
2023-12-31 15:50:13 -0500

    fix: installs

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
 });
